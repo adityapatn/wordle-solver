5ae8392d394a92dfd6831802b9b146b986b9e865
Aditya Patnaik <aditya.patnaik.email@gmail.com>
2025-05-22 09:17:19 -0700

    Started general solver

---
+++ general-solver.py
@@ -0,0 +1,4 @@
+#The purpose of this program is to build a general-purpose word solver that takes a partial input and returns a list of the words that it could be.
+
+partial = input("Enter partial word using * as a wildcard: ")
+word_length = len(partial)
