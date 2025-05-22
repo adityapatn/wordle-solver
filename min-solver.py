ac0081838666aa3f681a0f9299d84b2267b556cf
Aditya Patnaik <aditya.patnaik.email@gmail.com>
2025-05-22 09:17:49 -0700

    Changed yellow input to be comma-delimited list

--- min-solver.py
+++ min-solver.py
@@ -1,162 +1,126 @@
 #This program is incomplete. Do not use.
 #The purpose of this file is to create a simplified version of solver.py without any of the computer option and cleaned-up functions
-#I am unhappy with optsolve.py because its method is too slow and I dont wish to record every solve, I simply want the answer. Since I am building a wordle website in parallel to this program, I want to store accounts and solve data in the website. This program is intended only as a solve-accelerator
+#I am unhappy with optsolve.py because its method is too slow and I dont wish to record every solve, I simply want the answer. Since I am building a wordle website in parallel to this program, I want to store accounts and solve data in the website. This program is intended only as a solve-accelerator.
 
 import sys
-# Open the file in read mode
 
+# Open the file in read mode
+with open('shuffled_real_wordles.txt', 'r') as file:
+    word_list = file.read().split()
+
+letter_frequencies = {'e': 45.72, 'a': 36.3, 'r': 33.33, 'o': 27.96, 't': 27.03, 'l': 26.66, 'i': 24.88, 's': 24.81, 'n': 21.32, 'c': 17.69, 'u': 17.32, 'y': 15.76, 'd': 14.57, 'h': 14.42, 'p': 13.61, 'm': 11.72, 'g': 11.53, 'b': 10.42, 'f': 8.53, 'k': 7.79, 'w': 7.23, 'v': 5.67, 'z': 1.48, 'x': 1.37, 'q': 1.08, 'j': 1.0, "'": 0.0, '-': 0.0}
+
+word_length = 5
 included = []
 excluded = []
-partial_word = []
-yellow = ["", "", "", "", ""]
-possible_words = []
-word_scores = []
-solution = "treat"
-singles = []
 multiples = []
-final_guesses = []
-guesses = 0
-count = 0
-guesslist = []
-chars = []
-debug = False
-all_english = False
-all_fives = False
-
-def return_second(x): #returns the second item of a tuple, used in sorting the tuples in a list
-    return x[1]
-
-def reset():
-    global included, excluded, partial_word, yellow, singles, multiples
-    included = []
-    excluded = []
-    partial_word = ["*"] * len(solution)
-    yellow = ["", "", "", "", ""]
-    singles = []
-    multiples = []
+partial_word = ["*"] * word_length #list so is it is mutable as well as iterable
+yellow = [""] * word_length #list so each position can contain several letters
+singles = []
 
 def ask():
-    global included, excluded, partial_word, yellow, multiples
+    global included, excluded, multiples, partial_word, yellow, singles
     included_input = input("Enter any new included (yellow and green) letters: ").lower().strip()
     for i in included_input:
-        if included_input.count(i) > 1:
-            multiples.append(i)
-    for i in included_input:
         included.append(i)
+        if included_input.count(i) > 1 and not i in multiples:
+            multiples.append(i)
+
     excluded_input = input("Enter any new excluded (dark grey) letters: ").lower().strip()
     for i in excluded_input:
         excluded.append(i)
-    partial_word_input = input("Enter the letters of the word you know are in the correct position (all green letters), using * as a wildcard (no green in the column): " ).lower().strip()
-    if not partial_word_input:
-        partial_word_input = "*" * len(solution)
-    for i in range(len(solution)):
-        partial_word[i] = partial_word_input[i]
-    for i in range(len(solution)):
-        yellow[i] += input("Enter any new yellow letters in column " + str(i + 1) + ": ").lower().strip()
-
-def handle_word(i): #appends a word to possible_words, returns nothing
-    if len(i) != len(solution):
-        return
-    for j in excluded:
-        if j in i and not j in included: #we need to include a clause here that if the letter (represented by var j) is in both included and excluded for the word i, we shouldn't throw out the word.
-            return
-
-    for j in included:
-        if not j in i:
-            return
-
-    for j, k in zip(i, partial_word):
-        if not (k == "*" or j == k):
-            return
-
-    for j, k in zip(i, yellow): #i = crane, j = c, k = th
-        if k: #if there are any yellow letters in the column
-            for l in k: #iterate through every yellow letter in the column
-                if j == l: #if the first letter is one we know is yellow in that column, throw out i
-                    return
     
+    partial_word_input = input("Enter the letters of the word you know are in the correct position (all green letters), using * as a wildcard (no green in the column): " ).lower().strip()
+    while len(partial_word_input) < word_length:
+        partial_word_input += "*"
+    #partial_word_input is now a five-character string of letters and placeholders
+    for i in range(5):
+        char = partial_word_input[i]
+        if char.isalpha():
+            partial_word[i] = partial_word_input[i]
+
+    yellow_input = input("Enter the yellow letters in the word separated by commas: ").split(",")
+    print(yellow_input)
+    while len(yellow_input) < 5:
+        yellow_input.append("")
+    
+    for index in range(5):
+        yellow[index] += yellow_input[index].lower().strip()
+
+def check_word(word):
+    global included, excluded, multiples, partial_word, yellow, singles
     for j in included:
         if j in excluded:
             singles.append(j)
+
+    for letter in excluded:
+        if letter in word:
+            return False
+
+    for letter in included:
+        if not letter in word:
+            return False
+
+    for letter, solution_letter in zip(word, partial_word):
+        if not (solution_letter == "*" or letter == solution_letter): #not (A or B) is more computationally efficient than (not A) and (not B) because the interpreter will not evaluate the second condition of an or expression if the first is true.
+            return False
+
+    for j, k in zip(word, yellow): #i = crane, j = c, k = th
+        if k: #if there are any yellow letters in the column
+            for l in k: #iterate through every yellow letter in the column
+                if j == l: #if the letter is one we know is yellow in that column, throw out i
+                    return False
     
     for j in singles:
-        if i.count(j) > 1:
-            return
+        if word.count(j) > 1:
+            return False
     
     for j in multiples:
-        if i.count(j) < 2:
-            return
-
-    possible_words.append(i) #adds the word (if it passes all the checks) to possible_words
-
-def solve(): #possible_words is now full
-    global possible_words
-    possible_words = []
-    for i in word_list:
-        handle_word(i)
-
-letter_scores = {'e': 56.88, 'a': 43.31, 'r': 38.64, 'i': 38.45, 'o': 36.51, 't': 35.43, 'n': 33.92, 's': 29.23, 'l': 27.98, 'c': 23.13, 'u': 18.51, 'd': 17.25, 'p': 16.14, 'm': 15.36, 'h': 15.31, 'g': 12.59, 'b': 10.56, 'f': 9.24, 'y': 9.06, 'w': 6.57, 'k': 5.61, 'v': 5.13, 'x': 1.48, 'z': 1.39, 'j': 1.00, 'q': 1.00, "'": 1, "-": 1}
-score_list = list(letter_scores.items())
-
-def calculate_frequency(letter):
-    count = 0
-    for i in chars:
-        if i == letter:
-            count += 1
-    total = len(chars)
-    percent = round((count / total) * (1/0.00233), 2)
-    return percent
-
-letter_frequencies = {'e': 45.72, 'a': 36.3, 'r': 33.33, 'o': 27.96, 't': 27.03, 'l': 26.66, 'i': 24.88, 's': 24.81, 'n': 21.32, 'c': 17.69, 'u': 17.32, 'y': 15.76, 'd': 14.57, 'h': 14.42, 'p': 13.61, 'm': 11.72, 'g': 11.53, 'b': 10.42, 'f': 8.53, 'k': 7.79, 'w': 7.23, 'v': 5.67, 'z': 1.48, 'x': 1.37, 'q': 1.08, 'j': 1.0, "'": 0.0, '-': 0.0}
-alphabet = "abcdefghijklmnopqrstuvwxyz'-"
-frequency_list = list(letter_frequencies.items())
-
-
-frequency_list.sort(reverse=True, key=return_second)
-
-def populate_frequencies(): #goes through the entire alphabet and creates frequency scores for each letter, appends to and sorts frequency list
-    global frequency_list, letter_frequencies
-    if all_english or all_fives:
-        letter_frequencies = letter_scores
-        frequency_list = score_list
-    else:
-        for i in alphabet:
-            letter_frequencies[i] = calculate_frequency(i)
-
-def score_words(): #takes all words in possible_words and assigns scores to them, appends scores to word_scores
-    global word_scores
-    word_scores = []
-    for i in possible_words:
-        word_score = 0
-        letters_in_word = ""
-        for j in i:
-            if j not in letters_in_word:
-                word_score += letter_frequencies[j]
-                letters_in_word += j
-        word_score = round(word_score, 4)
-        word_scores.append(word_score)
-
-#word_scores is now full
-
-def print_words(): #outputs the sorted list of possible words and their scores
-    global final_guesses, guesses, count
-    print("")
-    print("%d total results out of %d possible answers (%3.2f%%)." % (len(possible_words), len(word_list), 100.0 * len(possible_words) / len(word_list)))
-    final_guesses = list(zip(possible_words, word_scores))
-    final_guesses.sort(reverse=True, key=return_second)
+        if word.count(j) < 2:
+            return False
     
-    for i in final_guesses[:10]: 
-        count += 1      
-        print("%2d: %s (%.2f)" % (count, i[0], i[1]))
+    #print(word)
+    return True
 
-def main():
-    populate_frequencies()
-    while True:
-        global solution
+def remove_duplicates(message):
+    seen = ""
+    new = ""
+    for char in message:
+        if char not in seen:
+            new += char
+        seen += char
+    
+    return new
+
+def solve():
+    possible_solutions = []
+    for possible_word in word_list:
+        if check_word(possible_word): #this word is a potential solution, score it
+            word_score = 0
+            for letter in remove_duplicates(possible_word):
+                word_score += letter_frequencies[letter]
+            possible_solutions.append([possible_word, word_score])
+    
+    #possible_solutions is now populated, sort it
+    possible_solutions = sorted(possible_solutions, key=lambda x: x[1]) #sorted sorts from least to greatest, but we need largest score first
+    possible_solutions.reverse()
+
+    #print the words to the user
+    percent = round(100 * len(possible_solutions) / len(word_list), 2)
+    print("\n%d total results out of %d possible answers (%.2f%%)." % (len(possible_solutions), len(word_list), percent))
+    for index in range(min(10, len(possible_solutions))):
+        print("%2d: %s (%.2f)" % (index + 1, possible_solutions[index][0], possible_solutions[index][1]))
+    if len(possible_solutions) == 1:
+        print("\nThanks for playing!\n")
+        sys.exit()
+    if len(possible_solutions) < 1:
+        print("\nAn error occurred.\n")
+        sys.exit()
+
+while True:
+    try:
         ask()
         solve()
-        score_words()
-        print_words()
-
-reset()
-main()
+    except KeyboardInterrupt:
+        print("\nThanks for playing!\n")
+        sys.exit()
